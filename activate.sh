70c5424b6e1e739471256931007d842290f68598
taptap <alexey.e.egorov@gmail.com>
2017-05-09 00:16:03 +0300

    fix activate.sh to automate unpacking of env tarball

--- activate.sh
+++ activate.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-export PATH=/usr/class/cs143/bin/:$PATH
+
+if [[ $COMPILERS_ACTIVATED ]]; then
+    echo "Already activated!"
+    exit 1;
+fi
+
+DIR=$(dirname "$0")
+
+! [[ -d "$DIR/cs143" ]] && tar zxvf "$DIR/student-dist.tar.gz"
+
+echo "Adding $DIR/cs143/bin/ to PATH..."
+export PATH="$DIR"/cs143/bin/:$PATH
 export PS1="<compilers>:$PS1"
+export COMPILERS_ACTIVATED=1
